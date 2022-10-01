7673627dd7415157a227f1b4d9c88f6dd03cd141
JeeIn-Shin <wldls0530@gmail.com>
2022-10-01 16:06:40 +0900

    initial JavaScript

---
+++ entity/example1.js
@@ -0,0 +1,47 @@
+let readline = require('readline');
+
+
+//let fruit = prompt("Which fruit you want to buy? ", "apple");
+//내가 뭘 이해를 잘못하고 있나???? 계속 defined만 뜨네;
+// let fruit = readline.createInterface({
+// 	input:process.stdin,
+// 	output:process.stdout
+// });
+
+// fruit.question("Which fruit you want to buy? ", function(answer) {
+
+//     let bag = {
+//         [fruit] : 5
+//     };
+
+// 	console.log(bag.answer);
+// 	fruit.close();
+// });
+
+// let user = {
+//     name : "Jin",
+//     age : "25"
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+
+// user.isAdmin = true;
+
+// delete user.age;
+
+// console.log(user.isAdmin);
+
+// let my_profile = {
+//     name : "Jin",
+//     age : "25",
+//     "good at programming?" : false
+// };
+
+// function yesOrNo (my_profile)   {
+//     if (my_profile["good at programming?"] == false)
+
+//     return "no";
+// }
+
+// console.log(yesOrNo(my_profile));
